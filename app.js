31fe8b103750ca53b52097db57e27b3303b7de83
Yerbol Irgaliyev <yerbol.irgaliyev@gmail.com>
2022-04-11 21:22:05 +0600

    add bootstrap package

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(cookieParser());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
