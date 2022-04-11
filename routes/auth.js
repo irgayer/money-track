996e8fca678bd01eb8449328f91494cfc1f286af
Yerbol Irgaliyev <yerbol.irgaliyev@gmail.com>
2022-04-11 21:22:30 +0600

    add auth pages

--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.route('/signin')
-    .get((req, res) => res.send('sign in page'))
+    .get((req, res) => res.render('auth/signin'))
     .post((req, res) => res.send('sign in proceed'));
 
 router.route('/signup')
-    .get((req, res) => res.send('sign up page'))
+    .get((req, res) => res.render('auth/signup'))
     .post((req, res) => res.send('sign up proceed'));
 
 module.exports = router;
