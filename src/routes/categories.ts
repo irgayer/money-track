c1f999a788a58828dbd187e97001ad7e4fa70902
Yerbol Irgaliyev <yerbol.irgaliyev@gmail.com>
2022-05-14 14:51:41 +0600

    deus ex machina

--- src/routes/categories.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-router.route('/')
-    .get((req, res) => res.send('categories page'))
-    .post((req, res) => res.send('categories create'))
-    .delete((req, res) => res.send('delete category'));
-
-module.exports = router;
--- src/routes/users.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-router.route('/settings')
-    .get((req, res) => res.send('settings page'))
-    .patch((req, res) => res.send('settings update proceed'));
-
-module.exports = router;
